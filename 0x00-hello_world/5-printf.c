959d09cb8d94c96541b8392da6ab4b051672e517
oluwatosin-oder12 <rachael.opawale@yahoo.com>
2022-03-11 05:36:18 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if exicted properly, non-zero otherwise
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
