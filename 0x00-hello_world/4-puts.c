320f1e3fa323266e05cf73d1145ed9a90cc4ea92
oluwatosin-oder12 <rachael.opawale@yahoo.com>
2022-03-11 05:45:38 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print a phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exicted properly, non-zero otherwise
+ */
+
+int main(void)
+{
+	puts("\Programming is like building a multilingual puzzle");
+	return (0);
+}
