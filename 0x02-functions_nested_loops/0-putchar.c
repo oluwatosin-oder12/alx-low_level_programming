403ebc7846eb03ab042ee8f6b529b340787b8ebc
oluwatosin-oder12 <rachael.opawale@yahoo.com>
2022-03-15 12:07:17 -0700

    new

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,13 +5,12 @@
  * Description: prints _putchar
  * Return: 0 (Success)
  */
-
 int main(void)
 {
 	char text[9] = "_putchar";
 	int i = 0;
 
-	for (i = 0; i < 8; i++;)
+	for (i = 0; i < 8; i++)
 	{
 	  _putchar(text[i]);
 	}
