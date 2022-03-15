6cea6f7130e596b2abe106534bdfbd41aac1b508
oluwatosin-oder12 <rachael.opawale@yahoo.com>
2022-03-15 12:24:53 -0700

    new

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,13 @@
  */
 int main(void)
 {
-	char text[9] = "_putchar";
+	char text[9] = "_putchar\n";
 	int i = 0;
 
 	for (i = 0; i < 8; i++)
 	{
 	  _putchar(text[i]);
 	}
-	_putchar('\n');
 
 	return (0);
 }
